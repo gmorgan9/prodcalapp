440bc57c6f6d71121bebdd24eb2a6ca3faed4540
Garrett Morgan <gar.mor.pro@outlook.com>
2022-02-27 23:45:22 -0700

    m

--- index.php
+++ index.php
@@ -44,6 +44,52 @@ session_start();
             7
         </div>
     </div>
+    <div class="row_1">
+        <div class="box_1">
+            8
+        </div>
+        <div class="box_2">
+            9
+        </div>
+        <div class="box_3">
+            10
+        </div>
+        <div class="box_4">
+            11
+        </div>
+        <div class="box_5">
+            12
+        </div>
+        <div class="box_6">
+            13
+        </div>
+        <div class="box_7">
+            14
+        </div>
+    </div>
+    <div class="row_1">
+        <div class="box_1">
+            15
+        </div>
+        <div class="box_2">
+            16
+        </div>
+        <div class="box_3">
+            17
+        </div>
+        <div class="box_4">
+            18
+        </div>
+        <div class="box_5">
+            19
+        </div>
+        <div class="box_6">
+            20
+        </div>
+        <div class="box_7">
+            21
+        </div>
+    </div>
 </div>
     
 </body>
