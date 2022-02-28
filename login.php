88fb65481632cdb7bb3ca445289daecdeea6251b
Garrett Morgan <gar.mor.pro@outlook.com>
2022-02-28 08:58:05 -0700

    m

--- login.php
+++ login.php
@@ -1,12 +1,38 @@
+<?php  ?>
+
+<?php
+
+?>
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login - Cal App</title>
+	<title>Login</title>
+	<meta name="viewport" content="width=device-width">
+	<link rel="stylesheet" href="styles.css?v=<?php echo time(); ?>">
 </head>
 <body>
-    
+	<div class="log-header">
+		<h2>Login - Cal App</h2>
+	</div>
+	<form method="post" action="login.php">
+
+		<!-- <?php echo display_error(); ?> -->
+
+		<div class="input-group">
+			<label>Username</label>
+			<input type="text" name="username" >
+		</div>
+		<div class="input-group">
+			<label>Password</label>
+			<input type="password" name="password">
+		</div>
+		<div class="input-group">
+			<button type="submit" class="log-btn" name="login_btn">Login</button>
+			<a href="javascript:history.back()" name="btn" class="log-btn" >Back</a>
+		</div>
+		<p>
+			Not yet a member? <a href="register.php">Sign up</a>
+		</p>
+	</form>
 </body>
 </html>
