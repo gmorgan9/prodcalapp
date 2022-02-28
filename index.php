eb13f572ce806abd9f6a936c0c67b2321412be45
Garrett Morgan <gar.mor.pro@outlook.com>
2022-02-28 09:09:37 -0700

    m

--- index.php
+++ index.php
@@ -9,6 +9,16 @@ session_start();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+    <!-- Font Awesome -->
+    <link rel="stylesheet"
+    href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
+    integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr"
+    crossorigin="anonymous">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Candal|Lora"
+    rel="stylesheet">
+
     <!-- Custom Styling -->
     <link rel="stylesheet" href="../../assets/css/style.css?v=<?php echo time(); ?>">
     
