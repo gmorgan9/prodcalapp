2ad3a768f71a3487f9d0b6d8bc6ab58c7549955f
Garrett Morgan <gar.mor.pro@outlook.com>
2022-02-27 23:28:29 -0700

    m

---
+++ app/includes/week_days.php
@@ -0,0 +1,39 @@
+<div class="cal">
+    <div class="week_day">
+        <div class="sun">
+            <h3>
+                Sunday
+            </h3>
+        </div>
+        <div class="mon">
+            <h3>
+                Monday
+            </h3>
+        </div>
+        <div class="tue">
+            <h3>
+                Tuesday
+            </h3>
+        </div>
+        <div class="wed">
+            <h3>
+                Wednesday
+            </h3>
+        </div>
+        <div class="thu">
+            <h3>
+                Thursday
+            </h3>
+        </div>
+        <div class="fri">
+            <h3>
+                Friday
+            </h3>
+        </div>
+        <div class="sat">
+            <h3>
+                Saturday
+            </h3>
+        </div>
+    </div>
+</div>
--- index.php
+++ index.php
@@ -18,46 +18,7 @@ session_start();
 
 <?php include(ROOT_PATH . "/app/includes/header.php"); ?>
 <hr>
-
-<div class="cal">
-    <div class="week_day">
-        <div class="sun">
-            <h3>
-                Sunday
-            </h3>
-        </div>
-        <div class="mon">
-            <h3>
-                Monday
-            </h3>
-        </div>
-        <div class="tue">
-            <h3>
-                Tuesday
-            </h3>
-        </div>
-        <div class="wed">
-            <h3>
-                Wednesday
-            </h3>
-        </div>
-        <div class="thu">
-            <h3>
-                Thursday
-            </h3>
-        </div>
-        <div class="fri">
-            <h3>
-                Friday
-            </h3>
-        </div>
-        <div class="sat">
-            <h3>
-                Saturday
-            </h3>
-        </div>
-    </div>
-</div>
+<?php include(ROOT_PATH . "/app/includes/week_days.php"); ?>
     
 </body>
 </html>
