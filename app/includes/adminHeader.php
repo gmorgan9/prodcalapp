1556d6dc7ae36860c832b6df2c45d0bfc71ea51d
Garrett Morgan <gar.mor.pro@outlook.com>
2022-02-28 00:41:28 -0700

    m

--- app/includes/adminHeader.php
+++ app/includes/adminHeader.php
@@ -3,12 +3,13 @@
         <h1>
         Admin - Calendar App
         </h1>
+        <li class="nav-list-item"><b>Department:</b> Geography</li>
     </div>
     <div class="nav-header">
         <ul class="nav-list">
             <li class="nav-list-item"><a class="nav-list-link" href="">Add Event</a></li>
-            <li class="nav-list-item"><b>Department:</b> Geography</li>
-            <li class="nav-list-item"><a class="nav-list-link" href="/admin/index.php">Admin Settings</a></li>
+            
+            <li class="nav-list-item"><a class="nav-list-link" href="/admin/index.php">Home</a></li>
             <li class="nav-list-item"><a class="nav-list-link" href="">Logout</a></li>
         </ul>
     </div>
