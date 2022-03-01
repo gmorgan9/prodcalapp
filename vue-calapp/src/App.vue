054fae9e6c3c8d1d8dedb94ee2b2c650021cdfa8
Garrett Morgan <gar.mor.pro@outlook.com>
2022-03-01 16:36:45 -0700

    m

--- vue-calapp/src/App.vue
+++ vue-calapp/src/App.vue
@@ -1,33 +1,31 @@
 <template>
-  <div id="app">
-    <b-navbar toggleable="lg" type="dark" variant="info">
-      <b-navbar-brand to="/">Calendar App</b-navbar-brand>
-      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-      <b-collapse id="nav-collapse" is-nav>
-        <b-navbar-nav>
-          <b-nav-item to="/" :active="path  == '/'">Home</b-nav-item>
-        </b-navbar-nav>
-      </b-collapse>
-    </b-navbar>
-    <router-view />
-  </div>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link> | 
+    <router-link to="/calendar">Calendar</router-link>
+    <router-view/>
+  </nav>
 </template>
-<script>
-export default {
-  data() {
-    return {
-      path: this.$route && this.$route.path
-    };
-  },
-  watch: {
-    $route(route) {
-      this.path = route.path;
-    }
-  }
-};
-</script>
-<style lang="scss">
-.page {
-  padding: 20px;
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
--- vue-calapp/src/views/HomeView.vue
+++ vue-calapp/src/views/HomeView.vue
@@ -1,60 +1,18 @@
 <template>
-  <div class="page">
-    <div class="buttons">
-      <b-button v-b-modal.add-modal>Add Calendar Event</b-button>
-    </div>
-    <full-calendar :events="events" @event-selected="openEditModal" defaultView="month" />
-<b-modal id="add-modal" title="Add Calendar Event" hide-footer ref="add-modal">
-      <CalendarForm :edit="false" @eventSaved="closeModal()" />
-    </b-modal>
-<b-modal id="edit-modal" title="Edit Calendar Event" hide-footer ref="edit-modal">
-      <CalendarForm :edit="true" :calendarEvent="calendarEvent" @eventSaved="closeModal()" />
-    </b-modal>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
+
 <script>
 // @ is an alias to /src
-import CalendarForm from "@/components/CalendarForm.vue";
-import { requestsMixin } from "../mixins/requestsMixin";
+import HelloWorld from '@/components/HelloWorld.vue'
+
 export default {
-  name: "homeview",
+  name: 'HomeView',
   components: {
-    CalendarForm
-  },
-  mixins: [requestsMixin],
-  computed: {
-    events() {
-      return this.$store.state.events;
-    }
-  },
-  data() {
-    return {
-      calendarEvent: {}
-    };
-  },
-  async beforeMount() {
-    await this.getEvents();
-  },
-  methods: {
-    async getEvents() {
-      const response = await this.getCalendar();
-      this.$store.commit("setEvents", response.data);
-    },
-    closeModal() {
-      this.$refs["add-modal"].hide();
-      this.$refs["edit-modal"].hide();
-      this.calendarEvent = {};
-    },
-    openEditModal(event) {
-      let { id, start, end, title } = event;
-      this.calendarEvent = { id, start, end, title };
-      this.$refs["edit-modal"].show();
-    }
+    HelloWorld
   }
-};
-</script>
-<style lang="scss" scoped>
-.buttons {
-  margin-bottom: 10px;
 }
-</style>
+</script>
