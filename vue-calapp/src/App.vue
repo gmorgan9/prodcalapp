ad2c36d698f3a0d06853a22c96416f0349a533d3
Garrett Morgan <gar.mor.pro@outlook.com>
2022-03-01 15:12:10 -0700

    m

--- vue-calapp/src/App.vue
+++ vue-calapp/src/App.vue
@@ -4,7 +4,7 @@
     <router-link to="/about">About</router-link> | 
     <router-link to="/calendar">Calendar</router-link>
   </nav>
-  <router-view/>
+  <!-- <router-view/> -->
 </template>
 
 <style>
