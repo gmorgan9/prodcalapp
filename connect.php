79fd58092d2ec70a93a0b09ceafa9760e663f3ad
Garrett Morgan <gar.mor.pro@outlook.com>
2022-02-28 23:17:36 -0700

    m

--- connect.php
+++ connect.php
@@ -2,7 +2,7 @@
 try {
 	$dsn = "pgsql:host=localhost;port=5432;dbname=postgres;";
 	// make a database connection
-	$pdo = new PDO($dsn, "postgres", "admin350PASS!", [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+	$pdo = new PDO($dsn, postgres, admin350PASS!, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
 
 	if ($pdo) {
 		echo "Connected to the $db database successfully!";
