8dad0f2a74c2af54f1f1850026907330bd48e6a1
Garrett Morgan <gar.mor.pro@outlook.com>
2022-03-01 16:30:17 -0700

    m

--- vue-calapp/src/router/index.js
+++ vue-calapp/src/router/index.js
@@ -1,30 +1,16 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-
-const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  },
-  {
-    path: '/calendar',
-    name: 'calendar',
-    component: () => import('../views/CalendarView.vue')
-  }
-]
-
-const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
-})
-
-export default router
+import Vue from "vue";
+import Router from "vue-router";
+import HomeView from "./views/HomeView.vue";
+import 'fullcalendar/dist/fullcalendar.css'
+Vue.use(Router);
+export default new Router({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: HomeView
+    }
+  ]
+});
