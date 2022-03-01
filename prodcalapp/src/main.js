d94e36566ca3e0bf3367155663e3b935ffd50ba0
Garrett Morgan <gar.mor.pro@outlook.com>
2022-03-01 10:43:24 -0700

    m

--- prodcalapp/src/main.js
+++ prodcalapp/src/main.js
@@ -17,7 +17,7 @@ Vue.config.productionTip = false;
 
 // import "bootstrap/dist/css/bootstrap.css";
 // import "bootstrap-vue/dist/bootstrap-vue.css";
-import { getJwtToken } from "./auth";
+// import { getJwtToken } from "./auth";
 
 // Vue.use(BootstrapVue);
 Vue.use(IconsPlugin);
