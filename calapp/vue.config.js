57e7675c20971b59edbb1b8b49dbe1b1b7844da2
Garrett Morgan <gar.mor.pro@outlook.com>
2022-02-28 21:27:49 -0700

    m

--- calapp/vue.config.js
+++
@@ -1,9 +0,0 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true,
-
-  pwa: {
-    name: 'Calendar App',
-    themeColor: '#3D89BA'
-  }
-})
