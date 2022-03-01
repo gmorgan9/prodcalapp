3f4a7e75815e186bb53129c3f954536a8d6f09b2
Garrett Morgan <gar.mor.pro@outlook.com>
2022-03-01 00:22:06 -0700

    m

--- connect.php
+++ connect.php
@@ -4,7 +4,7 @@ $db_handle = pg_connect("host=10.25.90.110 port=5432 dbname=calapp user=gmor pas
 
 if ($db_handle) {
 
-echo 'Connection attempt succeeded. I DID IT!!';
+echo 'Connection attempt succeeded. I DID IT!! test the connection';
 
 } else {
 
