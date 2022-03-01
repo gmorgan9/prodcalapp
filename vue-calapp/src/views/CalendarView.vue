358ca014f0c7624c1b5f801e392aabf74637f9b3
Garrett Morgan <gar.mor.pro@outlook.com>
2022-03-01 15:19:20 -0700

    m

--- vue-calapp/src/views/CalendarView.vue
+++ vue-calapp/src/views/CalendarView.vue
@@ -1,5 +1,25 @@
 <template>
   <div class="calendar">
     <h1>Calendar</h1>
+    <form @submit.prevent="handleSubmit">
+        <div class="input-holder">
+          <input type="text" placeholder="Event title" v-model="event.title"/>
+        </div>
+        <div class="input-holder">
+          <date-picker :placeholder="'Start date'" v-model="event.start" /> 
+        </div>
+        <div class="input-holder">
+          <date-picker :placeholder="'End date'" v-model="event.end"/> 
+        </div>
+        <div class="input-holder">
+          <textarea placeholder="Event description" rows="4" v-model="event.data.description" ></textarea>
+        </div>
+        <div class="input-holder">
+          <color-picker @colorPicked="selectColor" :color="event.cssClass" />
+        </div>
+        <div class="input-holder">
+          <button type="submit">Schedule</button>
+        </div>
+      </form>
   </div>
 </template>
