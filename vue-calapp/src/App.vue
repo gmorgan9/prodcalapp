87a795ddd89a3452ea176f0a75a5125f1053bb47
Garrett Morgan <gar.mor.pro@outlook.com>
2022-03-01 16:56:50 -0700

    m

--- vue-calapp/src/App.vue
+++ vue-calapp/src/App.vue
@@ -1,5 +1,13 @@
 <template>
+
+
 	<div id="app">
+    <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link> | 
+    <router-link to="/calendar">Calendar</router-link>
+    <router-view/>
+  </nav>
 		<div class="calendar-controls">
 			<div v-if="message" class="notification is-success">{{ message }}</div>
 
