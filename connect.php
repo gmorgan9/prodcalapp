f34a32ea26f86f672dbfaf70c7268a783476b49f
Garrett Morgan <gar.mor.pro@outlook.com>
2022-02-28 23:22:45 -0700

    m

--- connect.php
+++ connect.php
@@ -1,16 +1,10 @@
 <?php 
-$hostname = "localhost";
-$dbname = "CalendarApp";
-$username = "postgres";
-$pass = "admin350PASS!";
-
-// Create connection
-$db_conn = pg_connect("host = $hostname dbname = $dbname user = $username password = $pass");
-
-if(!$db_conn) {
-    echo "no";
-} else {
-    echo "yes";
-}
+$dbconn = pg_connect("host=localhost port=5432 dbname=postgres user=postgres password=admin350PASS!");  
+//connect to a database named "postgres" on the host "host" with a username and password  
+if (!$dbconn){  
+echo "<center><h1>Doesn't work =(</h1></center>";  
+}else  
+ echo "<center><h1>Good connection</h1></center>";  
+pg_close($dbconn);
 
   ?>
