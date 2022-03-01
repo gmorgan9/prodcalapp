f8db1e845f3133f61fc6aff014a7d75160213c53
Garrett Morgan <gar.mor.pro@outlook.com>
2022-02-28 22:59:02 -0700

    m

--- connect.php
+++ connect.php
@@ -1,8 +1,8 @@
 <?php 
-  $host        = "host = 10.25.90.110";
+  $host        = "host = 10.25.90.110/24";
   $port        = "port = 5432";
   $dbname      = "dbname = calapp";
-  $credentials = "user = gmor password=gmorpass";
+  $credentials = "user = gmor password = gmorpass";
 
   $db = pg_connect( "$host $port $dbname $credentials"  );
   if(!$db) {
