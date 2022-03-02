1c537c3241954e8ba730b88314827f983ecc84bc
Garrett Morgan <gar.mor.pro@outlook.com>
2022-03-02 08:25:15 -0700

    m

---
+++ calapp/src/api.js
@@ -0,0 +1,64 @@
+import axios from "axios";
+import { authHeader, getJwtToken, getUserIdFromToken } from "./auth";
+
+const API_URL = "http://10.25.90.110:8000";
+
+class Api {
+  getUsers() {
+    return axios.get(API_URL + "/users", {});
+  }
+
+  getArticleDetail(id) {
+    return axios.get(API_URL + `/articles?articleid=eq.${id}`);
+  }
+
+  addArticle(article) {
+    return axios.post(
+      API_URL + "/articles",
+      {
+        ...article,
+        // add user id from JWT token
+        user_id: getUserIdFromToken(getJwtToken()),
+      },
+      {
+        headers: authHeader(),
+      }
+    );
+  }
+
+  updateArticle(article) {
+    return axios.patch(
+      API_URL + `/articles?articleid=eq.${article.articleid}`,
+      article,
+      {
+        headers: authHeader(),
+      }
+    );
+  }
+
+  deleteArticle(id) {
+    return axios.delete(API_URL + `/articles?articleid=eq.${id}`, {
+      headers: authHeader(),
+    });
+  }
+
+  publishArticle(id) {
+    return axios.post(
+      API_URL + "/rpc/publish",
+      { id: id },
+      {
+        headers: authHeader(),
+      }
+    );
+  }
+
+  login(username, password_hash) {
+    return axios.post(API_URL + "/rpc/login", { username, password_hash });
+  }
+
+  signup(username, email, password, dept_id) {
+    return axios.post(API_URL + "/rpc/signup", { username, email, password, dept_id });
+  }
+}
+
+export default new Api();
--- calapp/src/views/HomeView.vue
+++ calapp/src/views/HomeView.vue
@@ -1,8 +1,36 @@
 <template>
-  <div class="home">
-    <h1>TESTING</h1>
+  <div>
+    <b-jumbotron>
+      <p>Welcome to the IT350 blog!</p>
+    </b-jumbotron>
+    <br />
+    <div v-if="loading">Loading articles....</div>
+    <ul v-else>
+      <li v-for="users in user" :key="users.id">
+        <router-link :to="`users/${users.id}`">{{
+          users.username
+        }}</router-link>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
+import Api from "../api";
+export default {
+  name: "Home",
+  data: function () {
+    return {
+      loading: false,
+      user: [],
+    };
+  },
+  created: function () {
+    this.loading = true;
+    Api.getUsers().then((res) => {
+      this.user = res.data;
+      this.loading = false;
+    });
+  },
+};
 </script>
