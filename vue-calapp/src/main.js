6e9e10c1b8dd2dc6390752c925fed0a582f2a811
Garrett Morgan <gar.mor.pro@outlook.com>
2022-03-01 19:07:18 -0700

    m

--- vue-calapp/src/main.js
+++ vue-calapp/src/main.js
@@ -1,6 +1,17 @@
-import { createApp } from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
+import AsyncComputed from 'vue-async-computed'
+import vuetify from './plugins/vuetify';
+import 'roboto-fontface/css/roboto/roboto-fontface.css'
+import '@mdi/font/css/materialdesignicons.css'
 
-createApp(App).use(router).mount('#app')
+Vue.config.productionTip = false
+
+Vue.use(AsyncComputed)
+new Vue({
+  router,
+  vuetify,
+  render: h => h(App)
+}).$mount('#app')
