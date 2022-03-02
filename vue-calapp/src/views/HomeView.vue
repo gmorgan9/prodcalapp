884f3176438e74a47d200b7978abdb4b840998f6
Garrett Morgan <gar.mor.pro@outlook.com>
2022-03-01 17:02:43 -0700

    m

--- vue-calapp/src/views/HomeView.vue
+++ vue-calapp/src/views/HomeView.vue
@@ -1,18 +1,36 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <b-jumbotron>
+      <p>Welcome to the IT350 blog!</p>
+    </b-jumbotron>
+    <br />
+    <div v-if="loading">Loading articles....</div>
+    <ul v-else>
+      <li v-for="article in articles" :key="article.articleid">
+        <router-link :to="`article/${article.articleid}`">{{
+          article.title
+        }}</router-link>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import Api from "../api";
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+  data: function () {
+    return {
+      loading: false,
+      articles: [],
+    };
+  },
+  created: function () {
+    this.loading = true;
+    Api.getArticles().then((res) => {
+      this.articles = res.data;
+      this.loading = false;
+    });
+  },
+};
 </script>
