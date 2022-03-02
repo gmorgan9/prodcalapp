c23527d887f54d43cd00e774d14f818bafe28e3d
Garrett Morgan <gar.mor.pro@outlook.com>
2022-03-01 17:06:49 -0700

    m

---
+++ vue-calapp/src/api.js
@@ -0,0 +1,64 @@
+import axios from "axios";
+import { authHeader, getJwtToken, getUserIdFromToken } from "./auth";
+
+const API_URL = "http://10.25.90.110:8000";
+
+class Api {
+  getArticles() {
+    return axios.get(API_URL + "/users", {});
+  }
+
+//   getArticleDetail(id) {
+//     return axios.get(API_URL + `/articles?articleid=eq.${id}`);
+//   }
+
+//   addArticle(article) {
+//     return axios.post(
+//       API_URL + "/articles",
+//       {
+//         ...article,
+//         // add user id from JWT token
+//         userid: getUserIdFromToken(getJwtToken()),
+//       },
+//       {
+//         headers: authHeader(),
+//       }
+//     );
+//   }
+
+//   updateArticle(article) {
+//     return axios.patch(
+//       API_URL + `/articles?articleid=eq.${article.articleid}`,
+//       article,
+//       {
+//         headers: authHeader(),
+//       }
+//     );
+//   }
+
+//   deleteArticle(id) {
+//     return axios.delete(API_URL + `/articles?articleid=eq.${id}`, {
+//       headers: authHeader(),
+//     });
+//   }
+
+//   publishArticle(id) {
+//     return axios.post(
+//       API_URL + "/rpc/publish",
+//       { id: id },
+//       {
+//         headers: authHeader(),
+//       }
+//     );
+//   }
+
+  login(user_name, password_hash) {
+    return axios.post(API_URL + "/rpc/login", { user_name, password_hash });
+  }
+
+  signup(user_name, user_email, password_hash) {
+    return axios.post(API_URL + "/rpc/signup", { user_name, user_email, password_hash });
+  }
+}
+
+export default new Api();
