54d09965d9074daa2e96595421df3e38dc5e9e2f
Garrett Morgan <gar.mor.pro@outlook.com>
2022-03-01 17:24:10 -0700

    m

--- vue-calapp/src/api.js
+++ vue-calapp/src/api.js
@@ -4,7 +4,7 @@ import { authHeader, getJwtToken, getUserIdFromToken } from "./auth";
 const API_URL = "http://10.25.90.110:8000";
 
 class Api {
-  getArticles() {
+  getUsers() {
     return axios.get(API_URL + "/users", {});
   }
 
