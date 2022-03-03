55971f7625264a49da46a442928e7bd22b9bd6ae
Garrett Morgan <gar.mor.pro@outlook.com>
2022-03-03 09:32:21 -0700

    m

--- calapp/src/views/CreateEvent.vue
+++ calapp/src/views/CreateEvent.vue
@@ -10,11 +10,25 @@
           <input v-model="title" placeholder="event title...">
           <input v-model="type" placeholder="event type...">
           <input v-model="type" placeholder="event type...">
-          <div class="date">
-            <label for="example-datepicker">Choose a date</label>
-            <b-form-datepicker id="example-datepicker" v-model="value" class="mb-2"></b-form-datepicker>
-            <p>Value: '{{ value }}'</p>
-          </div>
+          <div>
+    <b-form-group label="Select calendar interactive state" v-slot="{ ariaDescribedby }">
+      <b-form-radio-group
+        v-model="state"
+        :aria-describedby="ariaDescribedby"
+        aria-controls="ex-disabled-readonly"
+      >
+        <b-form-radio value="disabled">Disabled</b-form-radio>
+        <b-form-radio value="readonly">Readonly</b-form-radio>
+        <b-form-radio value="normal">Normal</b-form-radio>
+      </b-form-radio-group>
+    </b-form-group>
+
+    <b-calendar
+      id="ex-disabled-readonly"
+      :disabled="disabled"
+      :readonly="readonly"
+    ></b-calendar>
+  </div>
       </form>
 
 
