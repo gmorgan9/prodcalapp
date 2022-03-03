453b038cb66acedbe81fb819d0201a624c65b598
Garrett Morgan <gar.mor.pro@outlook.com>
2022-03-02 17:41:20 -0700

    m

--- calapp/src/views/HomeView.vue
+++ calapp/src/views/HomeView.vue
@@ -4,14 +4,14 @@
       <p>Welcome to the IT350 blog!</p>
     </b-jumbotron>
     <br />
-    <div v-if="loading">Loading articles....</div>
-    <ul v-else>
+    <!-- <div v-if="loading">Loading articles....</div> -->
+    <!-- <ul v-else> -->
       <li v-for="event in events" :key="event.id">
         <router-link :to="`/event/${event.id}`">{{
           event.title
         }}</router-link>
       </li>
-    </ul>
+    <!-- </ul> -->
   </div>
 </template>
 
@@ -21,15 +21,15 @@ export default {
   name: "Home",
   data: function () {
     return {
-      loading: false,
+      // loading: false,
       events: [],
     };
   },
   created: function () {
-    this.loading = true;
+    // this.loading = true;
     Api.getEvent().then((res) => {
       this.events = res.data;
-      this.loading = false;
+      // this.loading = false;
     });
   },
 };
