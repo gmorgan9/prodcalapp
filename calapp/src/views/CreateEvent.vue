fb699d6acd50ccadd854ca29b75a9f234b5910e2
Garrett Morgan <gar.mor.pro@outlook.com>
2022-03-03 09:33:34 -0700

    m

--- calapp/src/views/CreateEvent.vue
+++ calapp/src/views/CreateEvent.vue
@@ -10,25 +10,7 @@
           <input v-model="title" placeholder="event title...">
           <input v-model="type" placeholder="event type...">
           <input v-model="type" placeholder="event type...">
-          <div>
-    <b-form-group label="Select calendar interactive state" v-slot="{ ariaDescribedby }">
-      <b-form-radio-group
-        v-model="state"
-        :aria-describedby="ariaDescribedby"
-        aria-controls="ex-disabled-readonly"
-      >
-        <b-form-radio value="disabled">Disabled</b-form-radio>
-        <b-form-radio value="readonly">Readonly</b-form-radio>
-        <b-form-radio value="normal">Normal</b-form-radio>
-      </b-form-radio-group>
-    </b-form-group>
-
-    <b-calendar
-      id="ex-disabled-readonly"
-      :disabled="disabled"
-      :readonly="readonly"
-    ></b-calendar>
-  </div>
+          <mdb-date-picker-2 v-model="date" label="Basic example" title="Select date" />
       </form>
 
 
@@ -40,18 +22,19 @@
 <script>
 import Api from "../api";
 export default {
-    data() {
-      return {
-        value: ''
-      }
-    }
-  }
-//   created: function () {
-//     // this.loading = true;
-//     Api.getEvent().then((res) => {
-//       this.events = res.data;
-//       // this.loading = false;
-//     });
-//   },
-// };
+  name: "create",
+  data: function () {
+    return {
+      // loading: false,
+      events: [],
+    };
+  },
+  created: function () {
+    // this.loading = true;
+    Api.getEvent().then((res) => {
+      this.events = res.data;
+      // this.loading = false;
+    });
+  },
+};
 </script>
