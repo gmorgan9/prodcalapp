02a29860d1c98fa856501e6d50c185c6f5788998
Garrett Morgan <gar.mor.pro@outlook.com>
2022-03-03 14:26:30 -0700

    m

--- calapp/src/views/CreateEvent.vue
+++ calapp/src/views/CreateEvent.vue
@@ -9,7 +9,7 @@
       <form action="">
           <input v-model="title" placeholder="event title...">
           <input v-model="type" placeholder="event type...">
-          <datepicker :value="state.date"></datepicker>
+          <datepicker v-model="state.date" name="uniquename"></datepicker>
           <input v-model="time" placeholder="event time...">
           <input v-model="location" placeholder="event location...">
       </form>
