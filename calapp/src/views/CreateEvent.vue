d9a715931add6dda423367629c7b756ce32e275f
Garrett Morgan <gar.mor.pro@outlook.com>
2022-03-03 17:13:46 -0700

    m

--- calapp/src/views/CreateEvent.vue
+++ calapp/src/views/CreateEvent.vue
@@ -174,7 +174,7 @@ form {
 .location input,
 .description textarea {
   margin-left: 200px;
-  margin-top: -200px;
+  margin-top: -500px;
   width: 27.5%;
   /* margin-left: 70%; */
   /* padding-left: 2px; */
