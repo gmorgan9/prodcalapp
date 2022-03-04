f83d34858890d25a4cebd1ec4dbb9eacc0826a52
Garrett Morgan <gar.mor.pro@outlook.com>
2022-03-04 11:01:55 -0700

    m

--- calapp/src/views/AdminView.vue
+++ calapp/src/views/AdminView.vue
@@ -27,9 +27,7 @@ export default {
   name: "Admin",
   mounted: function () {
     authHeader();
-    return {
-      accessToken
-    }
+    this.$auth.getJwtToken({ accessToken })
   },
 };
 </script>
