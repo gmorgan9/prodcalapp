b2c514e19b1709072742561d6fe82da6ac2ee4c2
Garrett Morgan <gar.mor.pro@outlook.com>
2022-03-03 17:57:13 -0700

    m

--- calapp/src/main.js
+++ calapp/src/main.js
@@ -3,12 +3,12 @@ import VueRouter from "vue-router";
 import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 
 import App from "./App.vue";
-import Home from "./views/HomeView.vue";
+import Home from "./views/HomeView";
 // import Admin from "./pages/Admin";
 // import Login from "./pages/Login";
 // import Logout from "./pages/Logout";
 // import Register from "./pages/Register";
-import Event from "./views/EventView.vue";
+import Event from "./views/EventView";
 // import AdminArticleAdd from "./components/AdminArticleAdd";
 // import AdminArticleList from "./components/AdminArticleList";
 // import AdminArticleEdit from "./components/AdminArticleEdit";
