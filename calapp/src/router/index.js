8883d39835658ed7dd1d5281846e7f3de4b5e36b
Garrett Morgan <gar.mor.pro@outlook.com>
2022-03-03 17:58:35 -0700

    m

--- calapp/src/router/index.js
+++ calapp/src/router/index.js
@@ -1,32 +1,32 @@
-// import { createRouter, createWebHistory } from 'vue-router'
-// import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
 
-// const routes = [
-//   {
-//     path: '/',
-//     name: 'home',
-//     component: HomeView
-//   },
-//   {
-//     path: '/about',
-//     name: 'about',
-//     component: () => import('../views/AboutView.vue')
-//   },
-//   {
-//     path: '/event/:id',
-//     name: 'event',
-//     component: () => import('../views/EventView.vue')
-//   },
-//   {
-//     path: '/create',
-//     name: 'create',
-//     component: () => import('../views/CreateEvent.vue')
-//   }
-// ]
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/about',
+    name: 'about',
+    component: () => import('../views/AboutView.vue')
+  },
+  {
+    path: '/event/:id',
+    name: 'event',
+    component: () => import('../views/EventView.vue')
+  },
+  {
+    path: '/create',
+    name: 'create',
+    component: () => import('../views/CreateEvent.vue')
+  }
+]
 
-// const router = createRouter({
-//   history: createWebHistory(process.env.BASE_URL),
-//   routes
-// })
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
 
-// export default router
+export default router
