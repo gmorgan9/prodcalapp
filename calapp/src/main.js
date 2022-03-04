1f87e559fa5e3c0668e0c42428d752b8d07400b9
Garrett Morgan <gar.mor.pro@outlook.com>
2022-03-03 17:53:37 -0700

    m

--- calapp/src/main.js
+++ calapp/src/main.js
@@ -1,6 +1,62 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
-import router from './router'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 
-createApp(App).use(router).mount('#app')
+import App from "./App.vue";
+import Home from "./views/HomeView";
+// import Admin from "./pages/Admin";
+// import Login from "./pages/Login";
+// import Logout from "./pages/Logout";
+// import Register from "./pages/Register";
+import Event from "./view/EventView";
+import AdminArticleAdd from "./components/AdminArticleAdd";
+import AdminArticleList from "./components/AdminArticleList";
+import AdminArticleEdit from "./components/AdminArticleEdit";
+
+Vue.config.productionTip = false;
+
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+import { getJwtToken } from "./auth";
+
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
+Vue.use(VueRouter);
+
+const checkAuth = function(to, _, next) {
+  const token = getJwtToken();
+  if (token === undefined || token === "undefined" || token === null) {
+    // redirect to login because we don't have token yet
+    next({
+      path: "/login",
+      params: { nextUrl: to.fullPath },
+    });
+  } else {
+    next();
+  }
+};
+
+const router = new VueRouter({
+  routes: [
+    { path: "/", component: Home },
+    { path: "/event/:id", component: Event },
+    // { path: "/login", component: Login },
+    // { path: "/logout", component: Logout },
+    // { path: "/register", component: Register },
+    // {
+    //   path: "/admin",
+    //   component: Admin,
+    //   beforeEnter: checkAuth,
+    //   children: [
+    //     { path: "add", component: AdminArticleAdd },
+    //     { path: "edit/:id", component: AdminArticleEdit },
+    //     { path: "", component: AdminArticleList },
+    //   ],
+    // },
+  ],
+});
+
+new Vue({
+  router,
+  render: (h) => h(App),
+}).$mount("#app");
