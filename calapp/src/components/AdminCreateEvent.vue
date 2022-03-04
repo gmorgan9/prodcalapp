57d3fb976f3db6e6617424832829827ded12c841
Garrett Morgan <gar.mor.pro@outlook.com>
2022-03-04 13:46:42 -0700

    m

--- calapp/src/components/AdminCreateEvent.vue
+++ calapp/src/components/AdminCreateEvent.vue
@@ -132,10 +132,12 @@ export default {
       // Datepicker,
     };
   },
+  created: function() {
+    id = Api.getUserID()
+    console.log(id)
+},
   methods: {
     handleAdd() {
-      id = Api.getUserID()
-      console.log(id)
       this.loading = true;
       this.message = "";
       Api.addEvent({ user_id: this.user_id, title: this.title, type: this.type, date: this.date, time: this.time, location: this.location, description: this.description, cal_id: this.cal_id })
