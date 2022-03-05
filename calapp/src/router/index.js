c1243fb628239675027d644c97b8469b98290583
Garrett Morgan <gar.mor.pro@outlook.com>
2022-03-05 14:55:18 -0700

    m

--- calapp/src/router/index.js
+++ calapp/src/router/index.js
@@ -49,11 +49,11 @@ const routes = [
     component: Register
   },
   {
-    path: "/admin",
+    path: '/admin',
       component: Admin,
       beforeEnter: checkAuth,
       children: [
-        { path: "create", component: AdminCreateEvent },
+        { path: '/create', component: AdminCreateEvent },
         // { path: "edit/:id", component: AdminArticleEdit },
         // { path: "", component: AdminArticleList },
       ],
