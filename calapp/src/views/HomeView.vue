8b2c623d3104aa0221a985aa39231777ac7866df
Garrett Morgan <gar.mor.pro@outlook.com>
2022-03-05 14:34:46 -0700

    m

--- calapp/src/views/HomeView.vue
+++ calapp/src/views/HomeView.vue
@@ -5,7 +5,7 @@
     <ul v-else>
       <br /><br />
       <h1 v-if="{id}">Vue is awesome!</h1>
-    <h1 v-else>Oh no 😢</h1>
+      <h1 v-else>Oh no 😢</h1>
     <router-link class="nav-link" :to="`/login`">Login</router-link> | 
     <router-link class="nav-link" :to="`/register`">Register</router-link>
       <br /><br />
