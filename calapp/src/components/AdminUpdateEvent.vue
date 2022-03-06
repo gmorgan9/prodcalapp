8babf80dbdc92fcf3979db6b826b901faea5bc72
Garrett Morgan <gar.mor.pro@outlook.com>
2022-03-05 23:42:34 -0700

    m

--- calapp/src/components/AdminUpdateEvent.vue
+++ calapp/src/components/AdminUpdateEvent.vue
@@ -3,8 +3,8 @@
     <b-breadcrumb>
         <br>
         <h2>Update Event</h2>
-      <li class="breadcrumb-item" to="/admin"> Admin </b-breadcrumb-item>
-      <li class="breadcrumb-item" active>Edit Event</b-breadcrumb-item>
+      <li class="breadcrumb-item" to="/admin"> Admin </li>
+      <li class="breadcrumb-item" active>Edit Event</li>
     </b-breadcrumb>
     <div v-if="loading">Loading event...</div>
     <div v-else>
