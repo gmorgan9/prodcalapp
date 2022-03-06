fde5c262238db77bd09c11c026bc40949d2abf52
Garrett Morgan <gar.mor.pro@outlook.com>
2022-03-06 00:51:06 -0700

    m

--- calapp/src/views/AdminView.vue
+++ calapp/src/views/AdminView.vue
@@ -32,7 +32,7 @@
           event.title
         }}</router-link>
         <router-link id="update-events" class="event alert-primary" :to="`/update/${event.event_id}`">Update</router-link>
-        <button id="update-events" class="event alert-primary" @click="() => deleteEvent(event.event_id)">Delete</button>
+        <router-link id="update-events" class="event alert-primary" @click="() => deleteEvent(event.event_id)">Delete</router-link>
         </li>
       </ul>
 
