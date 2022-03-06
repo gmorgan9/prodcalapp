d971afa6572c87328a1a61bf4c7c3c0e57767414
Garrett Morgan <gar.mor.pro@outlook.com>
2022-03-06 11:22:59 -0700

    m

--- calapp/src/views/HomeView.vue
+++ calapp/src/views/HomeView.vue
@@ -27,6 +27,132 @@
 
 
 
+<div class="cal_day">
+    <div class="row_1">
+        <div class="box_1">
+            1
+        </div>
+        <div class="box_2">
+            2
+        </div>
+        <div class="box_3">
+            3
+        </div>
+        <div class="box_4">
+            4
+        </div>
+        <div class="box_5">
+            5
+        </div>
+        <div class="box_6">
+            6
+        </div>
+        <div class="box_7">
+            7
+        </div>
+    </div>
+    <div class="row_2">
+        <div class="box_1">
+            8
+        </div>
+        <div class="box_2">
+            9
+        </div>
+        <div class="box_3">
+            10
+        </div>
+        <div class="box_4">
+            11
+        </div>
+        <div class="box_5">
+            12
+        </div>
+        <div class="box_6">
+            13
+        </div>
+        <div class="box_7">
+            14
+        </div>
+    </div>
+    <div class="row_3">
+        <div class="box_1">
+            15
+        </div>
+        <div class="box_2">
+            16
+        </div>
+        <div class="box_3">
+            17
+        </div>
+        <div class="box_4">
+            18
+        </div>
+        <div class="box_5">
+            19
+        </div>
+        <div class="box_6">
+            20
+        </div>
+        <div class="box_7">
+            21
+        </div>
+    </div>
+    <div class="row_4">
+        <div class="box_1">
+            22
+        </div>
+        <div class="box_2">
+            23
+        </div>
+        <div class="box_3">
+            24
+        </div>
+        <div class="box_4">
+            25
+        </div>
+        <div class="box_5">
+            26
+        </div>
+        <div class="box_6">
+            27
+        </div>
+        <div class="box_7">
+            28
+        </div>
+    </div>
+    <div class="row_2">
+        <div class="box_1">
+            29
+        </div>
+        <div class="box_2">
+            30
+        </div>
+        <div class="box_3">
+            31
+        </div>
+        <div class="box_4">
+            1
+        </div>
+        <div class="box_5">
+            2
+        </div>
+        <div class="box_6">
+            3
+        </div>
+        <div class="box_7">
+            4
+        </div>
+    </div>
+</div>
+
+
+
+
+
+
+
+
+
 
 
 
