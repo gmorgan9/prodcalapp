10008e14f3727e0c455a9f7ede6faa9e237010cb
Garrett Morgan <gar.mor.pro@outlook.com>
2022-03-05 17:44:27 -0700

    m

--- calapp/src/components/AdminCreateEvent.vue
+++ calapp/src/components/AdminCreateEvent.vue
@@ -143,7 +143,7 @@ created: function() {
       Api.addEvent({ user_id: Api.getUserID(), title: this.title, type: this.type, date: this.date, time: this.time, location: this.location, description: this.description, cal_id: this.cal_id, reminder_amt: this.reminder_amt })
         .then(() => {
           this.loading = false;
-          this.$router.push("/admin/");
+          this.$router.push("/create/");
           this.savingSuccessful = true;
         })
         .catch((error) => {
