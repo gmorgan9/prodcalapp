d93de64c05ec381f20482776b77ade199228fa9d
Garrett Morgan <gar.mor.pro@outlook.com>
2022-03-07 13:42:00 -0700

    m

--- calapp/src/components/AdminViewUsers.vue
+++ calapp/src/components/AdminViewUsers.vue
@@ -3,12 +3,13 @@
     <br>
     <h2>Admin page</h2>
     <b-navbar>
-      <b-nav-item><router-link id="nav-link-route" :to="{ path: '/' }">Home</router-link></b-nav-item>
 
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
       <b-collapse id="nav-collapse" is-nav>
         <!-- Right aligned nav items -->
         <b-navbar-nav class="ml-auto">
+            <b-nav-item><router-link id="nav-link-route" :to="{ path: '/admin' }">Admin</router-link></b-nav-item>
+          | <b-nav-item><router-link id="nav-link-route" :to="{ path: '/' }">Admin</router-link></b-nav-item>
           | <b-nav-item><router-link id="nav-link-route" :to="{ path: '/logout' }">Logout</router-link></b-nav-item>
           | <b-nav-item><router-link id="nav-link-route" :to="{ path: '/create' }">Create</router-link></b-nav-item>
         </b-navbar-nav>
