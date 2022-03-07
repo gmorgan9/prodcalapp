8f41e226db45ee749b660ac64ee7681ceb6e5e68
Garrett Morgan <gar.mor.pro@outlook.com>
2022-03-07 12:27:15 -0700

    m

--- calapp/src/components/AdminCreateEvent.vue
+++ calapp/src/components/AdminCreateEvent.vue
@@ -90,9 +90,13 @@
               ></span>
               <span>Add Event</span>
             </button>
-            <a href="javascript:history.go(-1)"><button class="btn btn-primary btn-block">
-                Back
-            </button></a>
+            <router-link :to="`..`"><button class="btn btn-primary btn-block" :disabled="loading">
+                <span
+                  v-show="loading"
+                  class="spinner-border spinner-border-sm"
+                ></span>
+                <span>Back</span>
+            </button></router-link>
             
           </div>
       </form>
