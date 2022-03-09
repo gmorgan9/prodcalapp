fe136e1a35cc067275c804beabd8a3d72b32e84d
Garrett Morgan <gar.mor.pro@outlook.com>
2022-03-09 14:26:26 -0700

    m

--- calapp/src/router/index.js
+++ calapp/src/router/index.js
@@ -17,7 +17,9 @@ const checkAuth = function(to, _, next) {
 
 const checkAdmin = function(to, _, next) {
   const userid = Api.getUserID();
+  console.log(userid);
   const getuser = Api.getUsersDetail(userid);
+  console.log(getuser);
   if (getuser.isadmin == 0) {
     // redirect to login because we don't have token yet
     next({
