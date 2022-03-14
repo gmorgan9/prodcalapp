0ceb030ddab73dc6b3182bb3c320db9fd114889a
Garrett Morgan <gar.mor.pro@outlook.com>
2022-03-14 14:31:40 -0600

    m

--- calapp/src/views/SuperAdmin.vue
+++ calapp/src/views/SuperAdmin.vue
@@ -3,7 +3,7 @@
     <br>
     <h2>Admin page</h2>
     <b-navbar>
-      <b-nav-item><router-link id="nav-link-route" :to="{ path: '/' }">Home</router-link></b-nav-item>
+      <b-nav-item><router-link id="nav-link-route" :to="{ path: '/dashboard' }">Dashboard</router-link></b-nav-item>
 
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
       <b-collapse id="nav-collapse" is-nav>
@@ -29,6 +29,11 @@
     <router-link id="car-btn" :to="`/adminusers`"><button class="btn btn-primary">View</button></router-link>
   </div>
 </div>
+
+
+
+<div v-if="!isAdmin">HAPPY</div>
+<div v-else>NOT</div>
     
 </div>
 </template>
