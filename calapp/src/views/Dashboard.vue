5f1f9ad494072116addf56b2d8928675d506ba12
Garrett Morgan <gar.mor.pro@outlook.com>
2022-03-14 21:31:26 -0600

    m

--- calapp/src/views/Dashboard.vue
+++ calapp/src/views/Dashboard.vue
@@ -10,9 +10,9 @@
         <!-- Right aligned nav items -->
         <nav class="ml-auto">
           <router-link class="nav-link-route" :to="{ path: '/' }">Home</router-link>
-          | <router-link v-if="isAdmin" class="nav-link-route" :to="{ path: '/superadmin' }">Admin</router-link>
-          | <router-link class="nav-link-route" :to="{ path: '/logout' }">Logout</router-link>
-          | <router-link class="nav-link-route" :to="{ path: '/create' }">Create</router-link>
+          <router-link class="nav-link-route" :to="{ path: '/logout' }">Logout</router-link>
+          <router-link class="nav-link-route" :to="{ path: '/create' }">Create</router-link>
+          <router-link v-if="isAdmin" class="nav-link-route" :to="{ path: '/superadmin' }">Admin</router-link>
         </nav>
       </b-collapse>
     </b-navbar>
