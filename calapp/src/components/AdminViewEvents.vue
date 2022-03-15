05a3a030a9e27431746987d7d88b3bfa0f0fa59f
Garrett Morgan <gar.mor.pro@outlook.com>
2022-03-14 21:56:14 -0600

    m

--- calapp/src/components/AdminViewEvents.vue
+++ calapp/src/components/AdminViewEvents.vue
@@ -25,7 +25,7 @@
         <ul class="list-group">
           <li class="list-group-item" v-for="event in events" :key="event.event_id">
             <div class="title">{{ event.title }}</div>
-            <routerlink tag="li" exact v-on:click.native="deleteEvent(event.event_id)">Delete</routerlink>
+            <routerlink :to="{name: '/adminevents'}" tag="li" exact v-on:click.native="deleteEvent(event.event_id)">Delete</routerlink>
             <router-link id="update-events" :to="`/updateevents/${event.event_id}`">Update</router-link>
           </li>
         </ul>
