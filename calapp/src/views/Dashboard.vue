ba00dc3fc3fecb084626652555dc27fd8a6fb43f
Garrett Morgan <gar.mor.pro@outlook.com>
2022-03-14 22:14:11 -0600

    m

--- calapp/src/views/Dashboard.vue
+++ calapp/src/views/Dashboard.vue
@@ -9,10 +9,10 @@
       <b-collapse id="nav-collapse" is-nav>
         <!-- Right aligned nav items -->
         <nav>
-          <router-link class="nav-link-route" :to="{ path: '/' }">Home | </router-link>
-          <router-link class="nav-link-route" :to="{ path: '/logout' }">Logout | </router-link>
-          <router-link class="nav-link-route" :to="{ path: '/create' }">Create</router-link>
-          <router-link v-if="isAdmin" class="nav-link-route" :to="{ path: '/superadmin' }"> | Admin</router-link>
+          <router-link :to="{ path: '/' }">Home | </router-link>
+          <router-link :to="{ path: '/logout' }">Logout | </router-link>
+          <router-link :to="{ path: '/create' }">Create</router-link>
+          <router-link v-if="isAdmin" :to="{ path: '/superadmin' }"> | Admin</router-link>
         </nav>
       </b-collapse>
     </b-navbar>
