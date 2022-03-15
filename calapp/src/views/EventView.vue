2b391171e2a876752bfea2898f9f831ac910aba6
Garrett Morgan <gar.mor.pro@outlook.com>
2022-03-14 20:49:24 -0600

    m

--- calapp/src/views/EventView.vue
+++ calapp/src/views/EventView.vue
@@ -3,28 +3,28 @@
     <br>
     <div v-if="loading">Loading event....</div>
     <div v-else>
-      <h1 class="title">{{ event.title }}</h1>
-        <h3 class="type">{{ event.type }}</h3>
+      <h1 class="input-group mb-3">{{ event.title }}</h1>
+        <h3 class="input-group mb-3">{{ event.type }}</h3>
         <div class="content">
-        <div class="date_block">
+        <div class="input-group mb-3">
             <h3>Date</h3>
            <p> {{ event.date }} </p>
         </div>
-        <div class="time_block">
+        <div class="input-group mb-3">
             <h3>Time Of Day</h3>
             <p> {{ event.time }} </p>
         </div>
-        <div class="location_block">
+        <div class="input-group mb-3">
             <h3>Location</h3>
             <p v-if= "!!event.location"> {{ event.location }}</p>
             <p v-else>NONE</p>
         </div>
-        <div class="reminder_block">
+        <div class="input-group mb-3">
             <h3>Reminder</h3>
             <p v-if= "!!event.reminder"> {{ event.reminder }} Minute(s)</p>
             <p v-else>NONE</p>
         </div>
-        <div class="desc_block">
+        <div class="input-group mb-3">
             <h3>Description</h3>
             <p v-if= "!!event.description"> {{ event.description }}</p>
             <p v-else>NONE</p>
