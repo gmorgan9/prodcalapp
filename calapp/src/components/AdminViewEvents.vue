51fc8264d7d06248ba61b1e4ce9b8b55e271d480
Garrett Morgan <gar.mor.pro@outlook.com>
2022-03-14 21:47:04 -0600

    m

--- calapp/src/components/AdminViewEvents.vue
+++ calapp/src/components/AdminViewEvents.vue
@@ -25,8 +25,8 @@
         <ul class="list-group">
           <li class="list-group-item" v-for="event in events" :key="event.event_id">
             <div class="title">{{ event.title }}</div>
-            <button id="update-events-btn" class="btn btn-outline-danger" @click="() => deleteEvent(event.event_id)">Delete</button>
-            <router-link id="update-events" :to="`/updateevents/${event.event_id}`"><button class="btn btn-outline-success">Update</button></router-link>
+            <router-link @click="() => deleteEvent(event.event_id)">Delete</router-link>
+            <router-link id="update-events" :to="`/updateevents/${event.event_id}`">Update</router-link>
           </li>
         </ul>
   </div>
